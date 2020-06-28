37447a5a364d8a0904300e9dc31ee16ef81f662d
MaximilienRabourdin <maximilien.rabourdin@gmail.com>
2020-06-28 21:24:53 +0200

    Initial commit

---
+++ precache-manifest.c0b33a3fddf45892f8f45db95fd23f99.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "341e71750f54d4d9771404f4b984652e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "406ae864531ed421151c",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "643e9d566148f21bc487",
+    "url": "/static/js/2.bf5bad3d.chunk.js"
+  },
+  {
+    "revision": "df04310ad48f4f5411320a1942994090",
+    "url": "/static/js/2.bf5bad3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "406ae864531ed421151c",
+    "url": "/static/js/main.4359cb2c.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "7bf83b5b2f1cb1b2d081e72933bd91b2",
+    "url": "/static/media/Frame 2.7bf83b5b.png"
+  },
+  {
+    "revision": "85614008eaaf0bccdfe93b4b1377deed",
+    "url": "/static/media/ToDoList.85614008.png"
+  },
+  {
+    "revision": "7770c47e6991a0ab322c7b4e14a4a54b",
+    "url": "/static/media/max-mobile.7770c47e.png"
+  },
+  {
+    "revision": "b860c00a54b224a021329649f64d9f7c",
+    "url": "/static/media/maxi.b860c00a.png"
+  },
+  {
+    "revision": "1baba0956c57488bbc9745ea78be7c84",
+    "url": "/static/media/portfolio.1baba095.png"
+  },
+  {
+    "revision": "bae6af66cd9e8696600aed487111a409",
+    "url": "/static/media/serie.bae6af66.png"
+  }
+]);
